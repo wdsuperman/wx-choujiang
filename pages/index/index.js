b8cbfb17f9754921d48478879eb2862d2c9c948a
wdsuperman <425233548@qq.com>
2019-04-22 17:33:34 +0800

    only star

--- pages/index/index.js
+++ pages/index/index.js
@@ -7,14 +7,15 @@ Page({
     data: {
         goods: [{
                 id: 1,
-                image: '',
-                shangjia: '打卡56天领福利',
-                time: '9小时41分钟后 自动开奖',
-                add: true,
+                image: '', //商品图片
+                shangjia: '打卡56天领福利', // 右上角红框内字
+                time: '9小时41分钟后 自动开奖', // 开奖时间
+                add: true, //是否已参加
+                join:'仅限女性用户参与', // 参加性别限制
                 sp: [{
                         id: 1,
                         title: '奖项一',
-                        content: '飞利浦电动牙刷',
+                        content: '飞利浦电动牙刷大娃娃达瓦达瓦大大是发奥术大师多生',
                         num: '1'
                     },
                     {
@@ -32,6 +33,7 @@ Page({
                 shangjia: '打卡56天领福利',
                 time: '9小时41分钟后 自动开奖',
                 add: true,
+                join:'',
                 sp: [{
                         id: 1,
                         title: '奖项一',
@@ -58,6 +60,7 @@ Page({
                 shangjia: '打卡56天领福利',
                 time: '9小时41分钟后 自动开奖',
                 add: false,
+                join:'仅限女性用户参与',
                 sp: [{
                         id: 1,
                         title: '奖项一',
@@ -83,11 +86,15 @@ Page({
             })
         }
     },
+    jump(){
+        wx.navigateTo({
+            url: '../tyLottery/tyLottery',
+        })
+    },
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function(options) {
-
     },
 
     /**
